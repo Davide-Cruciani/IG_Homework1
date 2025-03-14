f9ae1569d6911f02b43b42f43426c31af105e8d7
Davide <davidecruciani0601@gmail.com>
2025-03-14 18:38:48 +0100

    Shift from iteration on bg to iteration on fg + blending

--- project1.js
+++ project1.js
@@ -4,35 +4,37 @@
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite( bgImg, fgImg, fgOpac, fgPos )
 {
-    bW = bgImg.width;
-    bH = bgImg.height;
-    fW = fgImg.width;
-    fH = fgImg.height;
-
-
-    a = new Array();
-    startH = Math.max(0, fgPos.y);
-    startW = Math.max(0, fgPos.x);
-    endH = Math.min(bW, fgPos.y+fH);
-    endW = Math.min(bW, fgPos.x+fW);
+    const BW = bgImg.width;
+    const BH = bgImg.height;
+    const FW = fgImg.width;
+    const FH = fgImg.height;
+    const xOffset = fgPos.x;
+    const yOffset = fgPos.y;
+    const startH = Math.max(0, yOffset);
+    const startW = Math.max(0, xOffset);
+    const endH = Math.min(BW, yOffset+FH);
+    const endW = Math.min(BW, xOffset+FW);
     
-    console.log("Background: ", bW, bH, bgImg.data.length);
-    console.log("Foreground: ", fW, fH, fgImg.data.length);
-    console.log(startW, endW, startH, endH);
+    console.log("Background: ", BW, BH, bgImg.data);
+    console.log("Foreground: ", FW, FH, fgImg.data);
+    console.log("W: ", startW, endW, endW-startW, "H: ", startH, endH, endH-startH);
     console.log(fgPos);
-    
+    console.log(fgOpac);
 
-    for (let w=startW;w<endW;w++){
-        for(let h=startH;h<endH;h++){
-            for(let ch=0;ch<4;ch++){
-                if((ch + h*4 + w*4*bH) in a) {
-                }
-                else {
-                    a.push((ch + h*4 + w*4*bH))
-                };
-                bgImg.data[ch + w*4 + h*4*bW] = 255
+    for (let w = 0;w<FW;w++){
+        for(let h=0;h<FH;h++){
+            let wPossInB = w+xOffset;
+            let hPossInB = h+yOffset;
+            const alphaf = fgImg.data[3 + w*4 + h*4*FW]*fgOpac/255;
+            const alphab = bgImg.data[3 + w*4 + h*4*BW]/255;
+            const alphac = alphaf + (1-alphaf)*alphab;
+            if(wPossInB<0 || wPossInB>=BW || hPossInB<0 || hPossInB>=BH) continue;
+            for(let ch=0;ch<3;ch++){
+                const valueFg = fgImg.data[ch + w*4 + h*4*FW];          
+                const valueBg = bgImg.data[ch + wPossInB*4 + hPossInB*4*BW];
+                bgImg.data[ch + wPossInB*4 + hPossInB*4*BW] = (valueFg*alphaf + valueBg*(1-(alphaf))*alphab)/alphac;
             }
+            bgImg.data[3 + wPossInB*4 + hPossInB*4*BW] = alphac * 255;
         }
     }
-    console.log(a[a.length-1])
 }
